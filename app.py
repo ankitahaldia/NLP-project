ca9e3cf4ca7e20da6d56ece27f7f00714b73237a
Davy Nimbona <davynimbona@gmail.com>
2021-02-17 12:54:15 +0100

    Flask file

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template
+import pickle
+import pipeline.model as model
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def home():
+
+    return "Alive!"
+
+
+@app.route('/industry', methods=["GET", "POST"])
+def predict_industry():
+    return "Welcome to the API Deployment"
