d6f9572fb65f092fbd20c1cf63a044ef3aa579a4
Davy Nimbona <davynimbona@gmail.com>
2021-02-18 10:36:34 +0100

    changes

--- stream_app.py
+++ stream_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from PIL import Image
 import pickle
+import PyPDF2
+from pipeline.preprocess.preprocess import Preprocess
 
 model = pickle.load(open('pipeline/model/clf.pickle', 'rb'))
 
@@ -10,12 +12,43 @@ def main():
     st.image(image, caption=None, width=100)
     st.markdown("###")
     st.header("Upload your document to be analyzed")
-    filename = st.file_uploader("Upload", type=["pdf", "txt", "jpg"])
+    filename = st.file_uploader("Upload", type=["pdf", "txt"])
     if filename is not None:
         if st.button("Analyze"):
-            tis = ["Hello", "World"]
-            st.write(tis[0])
-    
+            file_location = "assets/uploads/" + filename.name
+            with open(file_location, "wb") as f:
+                f.write(filename.getbuffer())
+                tis = ["Hello", "World"]
+                st.write(tis[0])
+            extracted_text = pdf_file_read(file_location)
+            st.write(type(extracted_text))
+            nlp_model = Preprocess()
+            st.write(nlp_model.preprocess(extracted_text))
+
+
+def pdf_file_read(file_name):
+    # creating a pdf file object
+    pdfFileObj = open(file_name, 'rb')  
+        
+    # creating a pdf reader object  
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)  
+        
+    # printing number of pages in pdf file  
+    print(pdfReader.numPages)  
+        
+    # creating a page object  
+    pageObj = pdfReader.getPage(0)  
+        
+    # extracting text from page  
+    text = pageObj.extractText() 
+        
+    # closing the pdf file object  
+    pdfFileObj.close()
+
+    return text
+
+
+# def extract_data()
 
 
 if __name__ == "__main__":
