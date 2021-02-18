50c9a56347e6a798ac94cfdcbfbd41d33565b50f
Davy Nimbona <davynimbona@gmail.com>
2021-02-18 17:02:17 +0100

    Hello

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 mkdir -p ~/.streamlit/
 python3 -m spacy download en_core_web_md
+
 echo "\
 [server]\n\
 port = $PORT\n\
@@ -7,3 +8,7 @@ enableCORS = false\n\
 headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
+
+python3 -m nltk.downloader 'punkt'
+python3 -m nltk.downloader 'averaged_perceptron_tagger'
+python3 -m nltk.downloader 'wordnet'
